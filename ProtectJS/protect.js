5bb40669ac4e9f26bc1b01436b70832e1cd7f859
TremayneChrist <Trem@MAYNE.local>
2014-01-09 09:01:38 +0000

    Change copy to fn

--- ProtectJS/protect.js
+++ ProtectJS/protect.js
@@ -19,12 +19,12 @@ var protect;
 	}
 
 	function protect_public(object, key) {
-		eval('var copy = ' + object.prototype[key].toString().replace(/\._/g, '._.'));
+		eval('var fn = ' + object.prototype[key].toString().replace(/\._/g, '._.'));
 		object.prototype[key] = function () {
 			publicResult = undefined;
 			calling = true;
 			try {
-				publicResult = copy.apply(this, arguments);
+				publicResult = fn.apply(this, arguments);
 			}
 			catch (e) {
 				calling = false;
@@ -36,13 +36,13 @@ var protect;
 	}
 
 	function protect_private(object, key) {
-		var copy = object.prototype[key];
+		var fn = object.prototype[key];
 		object.prototype._[key.substring(1)] = function () {
 
 			if (calling === false) {
 				throw 'You cannot call a private method';
 			}
-			return copy.apply(object, arguments);
+			return fn.apply(object, arguments);
 		}
 		delete object.prototype[key];
 	}
--- api.js
+++ api.js
@@ -21,26 +21,3 @@ var API = (function () {
 	return protect(API);
 })();
 
-var API2 = (function () {
-	function API2() {}
-
-	API2.prototype = {
-		public_1: function () {
-			console.log('I am a public method');
-			console.log('Calling private method(s)');
-			this._private_1('First call', '1');
-			this._private_1('Second call', '2');
-			this._private_1('Third call', '3');
-		},
-		public_2: function () {
-			console.log('I am public throw');
-			throw 'Uh oh!';
-		},
-		_private_1: function (a, b) {
-			console.log('I am private', a, b);
-		}
-	}
-
-	return API2;
-})();
-
