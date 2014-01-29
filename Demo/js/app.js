0d2ca17e70845d8dd9b79d7677568981c491ecce
Tremayne Christ <info@tremaynechrist.co.uk>
2014-01-29 07:42:24 +0000

    Added test

--- Demo/js/app.js
+++ Demo/js/app.js
@@ -2,3 +2,64 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
 	hljs.initHighlightingOnLoad();
 }
+function runTest() {
+	var TestObject = (function () {
+		function TestObject () {
+			this.testName = 'Protected object test';
+			this.step_6 = function () {
+				console.log('Step 6: Pass');
+			}
+			this.step_8 = function () {
+				console.log('Step 8: Pass');
+				this._end();
+			}
+			this._init();
+		}
+		TestObject.prototype = {
+			_init: function () {
+				console.log('Initialising - ' + this.testName);
+				this.step_1();
+			},
+			step_1: function () {
+				console.log('Step 1: Pass');
+				this.step_2();
+			},
+			step_2: function () {
+				console.log('Step 2: Pass');
+				this._step_3();
+			},
+			_step_3: function () {
+				var _this = this;
+				console.log('Step 3: Pass');
+				setTimeout(function () {
+					_this._step_4();
+				}, 300);
+			},
+			_step_4: function () {
+				var _this = this;
+				console.log('Step 4: Pass');
+				setTimeout(function () {
+					_this.step_5();
+				}, 600);
+			},
+			step_5: function () {
+				console.log('Step 5: Pass');
+				this.step_6();
+			},
+			step_7: function () {
+				var _this = this;
+				console.log('Step 7: Pass');
+				setTimeout(function () {
+					_this.step_8();
+				}, 1000);
+			},
+			_end: function () {
+				console.log(this.testName = ' completed');
+			}
+		}
+
+		return protect(TestObject);
+
+	})();
+	new TestObject();
+}
