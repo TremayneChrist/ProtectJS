803121fc2a595e34d266b3a958c465bc69339bd2
Tremayne Christ <info@tremaynechrist.co.uk>
2016-08-16 21:08:54 +0100

    Partially tidy up

--- protect.js
+++ protect.js
@@ -1,15 +1,22 @@
 (function () {
-  var callDepth = 0;
+
+  var callDepth = 0; // Stores the call depth of the current execution
+
   var locked = function () {
     return !callDepth;
   };
+
+  // The protect object
   var protect = function (_O) {
-    var O = _O.prototype || _O;
-    Object.keys(O)
-    .forEach(function (key) {
-        var original = O[key];
-        var isFunction = typeof original === 'function';
-        if (key.indexOf('_') === 0) {
+
+    var O = _O.prototype || _O; // Protect the prototype, if there is one
+
+    Object.keys(O).forEach(function (key) {
+
+        var value = O[key]; // The original property value
+        var isFunction = typeof value === 'function';
+
+        if (key.indexOf('_') === 0) { // Should the object be protected
           Object.defineProperty(O, key, {
             get: function () {
               if (locked()) {
@@ -17,32 +24,32 @@
               }
               else {
                 if (!isFunction) {
-                  return original;
+                  return value;
                 }
                 return function () {
-                  return original.apply(this, arguments);
+                  return value.apply(this, arguments);
                 };
               }
             },
             set: function () {
               if (!locked()) {
-                original = arguments[0];
+                value = arguments[0];
               }
             },
-            enumerable: false
+            enumerable: false // Remove from Object.keys
           });
         }
-        else {
+        else { // Allow functions to retrieve private properties
           Object.defineProperty(O, key, {
             get: function () {
               if (!isFunction) {
-                return original;
+                return value;
               }
               return function () {
                 var result;
                 try {
                   callDepth++;
-                  result = original.apply(this, arguments);
+                  result = value.apply(this, arguments);
                 }
                 catch (e) {
                   throw e;
@@ -54,14 +61,16 @@
               };
             },
             set: function () {
-              original = arguments[0];
+              value = arguments[0];
             }
           });
         }
     });
   };
+
+  // Make it available for both Node and Browser
   if( typeof exports !== 'undefined' ) {
-    if( typeof module !== 'undefined' && module.exports ) {
+    if( typeof module !== 'undefined' && module.exports) {
       exports = module.exports = protect;
     }
     exports.protect = protect;
@@ -69,4 +78,5 @@
   else {
     this.protect = protect;
   }
+
 }).call(this);
